6b9dbb5fc3bd5156eadfd2fb7b62e1c37babde5d
Martin Honnen <martin.honnen@gmx.de>
2022-09-10 11:06:15 +0200

    Projektdateien hinzufügen.

---
+++ SchxsltJavaIKVMNet6DefaultSaxon/Program.cs
@@ -0,0 +1,31 @@
+﻿using javax.xml.transform.dom;
+using javax.xml.transform.stream;
+using net.sf.saxon;
+using System.Reflection;
+using JFile = java.io.File;
+
+//Console.WriteLine(string.Join(Environment.NewLine, Assembly.Load("schxslt").GetManifestResourceNames()));
+//load Schxslt assembly with the necessary Schxslt XSLT files
+ikvm.runtime.Startup.addBootClassPathAssembly(Assembly.Load("schxslt"));
+
+var schematron = new name.dmaus.schxslt.Schematron(new StreamSource(new JFile(args[0])), null, new TransformerFactoryImpl());
+
+var result = schematron.validate(new StreamSource(new JFile(args[1])));
+
+Console.WriteLine($"{args[1]} valid against {args[0]}: {result.isValid()}");
+
+if (!result.isValid())
+{
+    foreach (var valMsg in result.getValidationMessages().toArray())
+    {
+        Console.WriteLine(valMsg);
+    }
+
+    Console.WriteLine("=======");
+    Console.WriteLine("Validation report:");
+
+    var identityTransformer = new TransformerFactoryImpl().newTransformer();
+    identityTransformer.setOutputProperty("indent", "yes");
+    
+    identityTransformer.transform(new DOMSource(result.getValidationReport()), new StreamResult(java.lang.System.@out));
+}
